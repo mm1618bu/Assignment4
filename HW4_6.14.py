6567742f1f55f996b56fda6d33d2d8e6cb499fde
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-11-07 22:02:58 -0500

    Update HW4_6.14.py

--- HW4_6.14.py
+++ HW4_6.14.py
@@ -1,13 +1,15 @@
+# numbers that will be passed through
 n = {1,101,201,301,401,501,601,701,801,901}
 
+#define the function to get pi
 def calcPi(n):
-
+    #set the value of pi and the value of numer
     pi,numer = 0,4.0
 
     for i in range(n):
-
+        # for each item in n, multiply by 2 and add 1
         denom = (2*i+1)
-
+        # get the result by dividing the numerator and denominator
         term = numer/denom
 
         if i%2:
@@ -20,7 +22,7 @@ def calcPi(n):
 
     return(pi)
 
-  
+#print the results in a table
 print("i"," ","m(i)")
 for i in n:
     print(i," ",calcPi(i))
